d01e2e7b5422f5e58aad6d9d6b61ba8a3832a601
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2024-10-21 20:37:25 -0300

    Add files via upload

--- js/icone.js
+++ js/icone.js
@@ -17,29 +17,40 @@ if (savedTheme) {
   }
 }
 
-// Adiciona o evento de clique ao ícone
-themeIcon.addEventListener('click', function() {
-  // Alterna entre o tema claro e escuro
-  if (body.classList.contains('light-theme')) {
-    body.classList.remove('light-theme');
-    body.classList.add('dark-theme');
-    localStorage.setItem('theme', 'dark-theme'); // Salva o tema atual
-    themeIcon.classList.add('hidden'); // Esconde o ícone de modo claro
-    lightIcon.classList.remove('hidden'); // Mostra o ícone de modo escuro
-  } else {
+
+
+// Verifica se os ícones existem antes de adicionar o evento
+if (themeIcon && lightIcon) {
+  // Evento para alternar para modo escuro
+  themeIcon.addEventListener('click', function() {
+    if (body.classList.contains('light-theme')) {
+      body.classList.remove('light-theme');
+      body.classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark-theme');
+      themeIcon.classList.add('hidden');
+      lightIcon.classList.remove('hidden');
+    }
+  });
+
+  // Evento para alternar para modo claro
+  lightIcon.addEventListener('click', function() {
     body.classList.remove('dark-theme');
     body.classList.add('light-theme');
-    localStorage.setItem('theme', 'light-theme'); // Salva o tema atual
-    themeIcon.classList.remove('hidden'); // Mostra o ícone de modo claro
-    lightIcon.classList.add('hidden'); // Esconde o ícone de modo escuro
-  }
-});
+    localStorage.setItem('theme', 'light-theme');
+    themeIcon.classList.remove('hidden');
+    lightIcon.classList.add('hidden');
+  });
 
-// Adiciona o evento de clique ao ícone de modo escuro
-lightIcon.addEventListener('click', function() {
-  body.classList.remove('dark-theme');
-  body.classList.add('light-theme');
-  localStorage.setItem('theme', 'light-theme'); // Salva o tema atual
-  themeIcon.classList.remove('hidden'); // Mostra o ícone de modo claro
-  lightIcon.classList.add('hidden'); // Esconde o ícone de modo escuro
-});
+  // Carrega o tema salvo do localStorage
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    body.classList.add(savedTheme);
+    if (savedTheme === 'dark-theme') {
+      themeIcon.classList.add('hidden');
+      lightIcon.classList.remove('hidden');
+    } else {
+      themeIcon.classList.remove('hidden');
+      lightIcon.classList.add('hidden');
+    }
+  }
+}
