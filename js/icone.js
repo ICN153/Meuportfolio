2eac78f2b68eaa4691b3545c097dc7b6a3932909
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2024-10-26 14:04:50 -0300

    Add files via upload

--- js/icone.js
+++ js/icone.js
@@ -2,12 +2,12 @@ const themeIcon = document.getElementById('theme-icon'); // Ícone de modo claro
 const lightIcon = document.getElementById('light-icon'); // Ícone de modo escuro
 const body = document.body;
 
-// Verifica se há um tema salvo no localStorage
+// Carrega o tema salvo do localStorage e aplica
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme) {
   body.classList.add(savedTheme);
   
-  // Verifica qual tema está ativo e ajusta os ícones
+  // Ajusta os ícones com base no tema salvo
   if (savedTheme === 'dark-theme') {
     themeIcon.classList.add('hidden'); // Esconde o ícone de modo claro
     lightIcon.classList.remove('hidden'); // Mostra o ícone de modo escuro
@@ -17,19 +17,15 @@ if (savedTheme) {
   }
 }
 
-
-
 // Verifica se os ícones existem antes de adicionar o evento
 if (themeIcon && lightIcon) {
   // Evento para alternar para modo escuro
   themeIcon.addEventListener('click', function() {
-    if (body.classList.contains('light-theme')) {
-      body.classList.remove('light-theme');
-      body.classList.add('dark-theme');
-      localStorage.setItem('theme', 'dark-theme');
-      themeIcon.classList.add('hidden');
-      lightIcon.classList.remove('hidden');
-    }
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark-theme');
+    themeIcon.classList.add('hidden');
+    lightIcon.classList.remove('hidden');
   });
 
   // Evento para alternar para modo claro
@@ -40,17 +36,4 @@ if (themeIcon && lightIcon) {
     themeIcon.classList.remove('hidden');
     lightIcon.classList.add('hidden');
   });
-
-  // Carrega o tema salvo do localStorage
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme) {
-    body.classList.add(savedTheme);
-    if (savedTheme === 'dark-theme') {
-      themeIcon.classList.add('hidden');
-      lightIcon.classList.remove('hidden');
-    } else {
-      themeIcon.classList.remove('hidden');
-      lightIcon.classList.add('hidden');
-    }
-  }
 }
