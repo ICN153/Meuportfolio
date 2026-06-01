835ffe67d003f8559c1fe0b4fb1ba633e56334de
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2026-06-01 18:59:10 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,25 +1,35 @@
-
-const btnMenu = document.querySelector('.btn-menu');
-const menuHeader = document.querySelector('.menu-header');
-const menuLinks = document.querySelectorAll('.menu-header a'); // Seleciona todos os links do menu
+const btnMenu = document.querySelector(".btn-menu");
+const menuHeader = document.querySelector(".menu-header");
+const menuLinks = document.querySelectorAll(".menu-header a"); // Seleciona todos os links do menu
 
 // Função que alterna o menu
 function toggleMenu() {
-    console.log("Menu button clicked!");
-    menuHeader.classList.toggle('menu-header-open');
-    btnMenu.classList.toggle('x');
+  console.log("Menu button clicked!");
+  menuHeader.classList.toggle("menu-header-open");
+  btnMenu.classList.toggle("x");
 }
 
 // Função para fechar o menu
 function closeMenu() {
-    menuHeader.classList.remove('menu-header-open'); // Fecha o menu
-    btnMenu.classList.remove('x'); // Reseta o ícone do botão
+  menuHeader.classList.remove("menu-header-open"); // Fecha o menu
+  btnMenu.classList.remove("x"); // Reseta o ícone do botão
 }
 
 // Adiciona evento de clique ao botão do menu
-btnMenu.addEventListener('click', toggleMenu);
+btnMenu.addEventListener("click", toggleMenu);
 
 // Adiciona evento de clique a cada link do menu
-menuLinks.forEach(link => {
-    link.addEventListener('click', closeMenu);
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
 });
+
+const cursor = document.querySelector(".cursor");
+
+const popup = document.getElementById("popup");
+const closePopup = document.getElementById("closePopup");
+
+if (closePopup && popup) {
+  closePopup.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+}
