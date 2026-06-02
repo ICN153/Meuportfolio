d925216333455703704eb732f89c6fd3c16c28b0
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2026-06-02 18:13:49 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,35 +1,37 @@
 const btnMenu = document.querySelector(".btn-menu");
 const menuHeader = document.querySelector(".menu-header");
-const menuLinks = document.querySelectorAll(".menu-header a"); // Seleciona todos os links do menu
+const menuLinks = document.querySelectorAll(".menu-header a");
 
-// Função que alterna o menu
 function toggleMenu() {
-  console.log("Menu button clicked!");
   menuHeader.classList.toggle("menu-header-open");
   btnMenu.classList.toggle("x");
 }
 
-// Função para fechar o menu
 function closeMenu() {
-  menuHeader.classList.remove("menu-header-open"); // Fecha o menu
-  btnMenu.classList.remove("x"); // Reseta o ícone do botão
+  menuHeader.classList.remove("menu-header-open");
+  btnMenu.classList.remove("x");
 }
 
-// Adiciona evento de clique ao botão do menu
 btnMenu.addEventListener("click", toggleMenu);
 
-// Adiciona evento de clique a cada link do menu
 menuLinks.forEach((link) => {
   link.addEventListener("click", closeMenu);
 });
 
 const cursor = document.querySelector(".cursor");
 
+/* POPUP */
 const popup = document.getElementById("popup");
 const closePopup = document.getElementById("closePopup");
 
-if (closePopup && popup) {
+if (popup) {
+  setTimeout(() => {
+    popup.classList.add("show");
+  }, 3000); // 5 segundos
+}
+
+if (closePopup) {
   closePopup.addEventListener("click", () => {
-    popup.style.display = "none";
+    popup.classList.remove("show");
   });
 }
